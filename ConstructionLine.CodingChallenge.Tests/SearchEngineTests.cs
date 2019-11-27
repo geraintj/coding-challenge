94706251b0e721b4bd97f9bc31f61d24882b7cad
Miloud Mostefaoui <m.mostefaoui@hotmail.fr>
2019-11-27 14:03:33 +0000

    Change the size from large to small

--- ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -22,7 +22,7 @@ namespace ConstructionLine.CodingChallenge.Tests
             var searchOptions = new SearchOptions
             {
                 Colors = new List<Color> {Color.Red},
-                Sizes = new List<Size> {Size.Large}
+                Sizes = new List<Size> {Size.Small}
             };
 
             var results = searchEngine.Search(searchOptions);
